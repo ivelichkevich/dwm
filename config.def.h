cfe6cc8de4dcb91d132de7e9261352c8a9902fef
ihar.velichkevich <igor.velichkevich@gmail.com>
2020-10-19 22:14:45 +0800

    font size and BROWSER

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=8" };
+static const char dmenufont[]       = "monospace:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -101,7 +101,8 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },
 	{ MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
 	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
-	{ MODKEY,			XK_w,	   spawn,	   SHCMD("$BROWSER") },
+	{ MODKEY,			            XK_w,	   spawn,	       SHCMD("echo $BROWSER > ~/w") },
+//	{ MODKEY,			            XK_w,	   spawn,	       SHCMD("$BROWSER") },
 };
 
 /* button definitions */
