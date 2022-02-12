407323aecce335019ca39104cfb9dbd479cf753a
Ihar Velichkevich <igor.velichkevich@gmail.com>
2022-02-12 08:42:36 +0800

    del workaround for libxft, now emoji works

---
+++ act_win.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ #xprop -id $(xdotool getactivewindow) |awk '/WM_CLASS/{print $4}' >> /home/ihar/act_win
+#xdotool getactivewindow >> /home/ihar/act_win
+#echo "|" >> /home/ihar/act_win
+#xprop -id $(xdotool getactivewindow) >> /home/ihar/act_win
+#echo "|" >> /home/ihar/act_win
+xdotool getwindowfocus getwindowname  >> /home/ihar/act_win
